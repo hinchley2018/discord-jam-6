66afc5678b7f6966f5a7eeefe67d123d7165fc8f
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2021-08-07 01:15:05 -0500

    implement bank

---
+++ Assets/Scripts/Bank.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bank : MonoBehaviour
+{
+    [SerializeField] private int income;
+    private bool exists = true;
+    private void Start()
+    {
+        StartCoroutine(EarnIncome());
+    }
+
+    private IEnumerator EarnIncome()
+    {
+        //initial delay
+        while (exists)
+        {
+            yield return new WaitForSeconds(2);
+            var _currencyManager = FindObjectOfType<CurrencyManager>();
+            _currencyManager.AddReward(5);
+            yield return new WaitForSeconds(2);
+        }
+        
+    }
+    
+}
