9c3cdbccf8dcb8d1f61413cf6579efa5a228a164
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2021-08-07 00:28:10 -0500

    working on damage over time

--- Assets/Scripts/Snowman.cs
+++ Assets/Scripts/Snowman.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System;
 using UnityEngine;
 
@@ -5,13 +6,18 @@ public class Snowman : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private int reward;
+    [SerializeField] private int health;
     [SerializeField] private Transform target;
     [SerializeField] private AudioClip spawnSound;
+    private bool IsInFactoryDamageRange = false;
+    private float delay = .25F;
 
     private void Start()
     {
         target = FindObjectOfType<TownCenter>().transform;
         AudioPlayer.PlaySound(spawnSound);
+
+        //StartCoroutine(CheckForDamage());
     }
 
     private void Update()
@@ -19,17 +25,40 @@ public class Snowman : MonoBehaviour
         var direction = target.position - transform.position;
         direction = direction.normalized;
         transform.position += direction * (speed * Time.deltaTime);
+        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.GetComponent<Factory>())
         {
-            var _currencyManager = FindObjectOfType<CurrencyManager>();
-            _currencyManager.AddReward(reward);
+            IsInFactoryDamageRange = true;           
             
-            Destroy(gameObject);
         }
             
     }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<Factory>())
+        {
+            IsInFactoryDamageRange = false;
+        }
+    }
+
+    private IEnumerator CheckForDamage()
+    {
+        while (IsInFactoryDamageRange)
+        {
+            if (health == 0)
+            {
+                var _currencyManager = FindObjectOfType<CurrencyManager>();
+                _currencyManager.AddReward(reward);
+    
+                Destroy(gameObject);
+            }
+            health -= 1;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+    
 }
